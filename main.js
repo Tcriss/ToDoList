f72f33568bb3692c610321dc428ba07128940ce3
Cristian Tejeda <88786977+Tcriss@users.noreply.github.com>
2022-05-11 22:25:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,22 +2,29 @@ const input = document.querySelector('input');
 const addBtn = document.querySelector('.btnadd');
 const ul = document.querySelector('ul');
 const empty = document.querySelector('.empty');
+const message = document.querySelector('.li-container');
+let tasks = [];
+let id = Date.now();
+
+showTasks();
 
 addBtn.addEventListener("click", (e) => {
-    //Prevenir que la pagina recargue
     e.preventDefault();
-
-    //Recoge el texto del campo
     const text = input.value;
 
     if (text !== "") {
-        //aqui se crea un nuevo list item
-        const li = document.createElement('li');
-        //se crea un parrafo para ser trasladado al list item anterior
+
+        const task = {
+            text,
+            id
+        }
+        tasks = [...tasks,task]
+
+        localStorage.setItem('tasks',JSON.stringify(tasks));
         const p = document.createElement('p');
         p.textContent = text;
-        
-        //aqui finalmente se inserta el Li
+              
+        const li = document.createElement('li');
         li.appendChild(p);
         li.appendChild(DeleteBtn());
         ul.appendChild(li);
@@ -25,28 +32,45 @@ addBtn.addEventListener("click", (e) => {
         input.value = "";
         empty.style.display = "none";
     } else {
-        alert("No puedes agregar un campo vacio");
+        alert("No se pueden crear tareas vacias");
     }
-    
 });
 
-function DeleteBtn() {
-    
+function DeleteBtn(e) {
     const deleteBtn = document.createElement("button");
     
     deleteBtn.textContent = "x";
+    deleteBtn.setAttribute("id", id);
     deleteBtn.className = "btndelete";
     
     deleteBtn.addEventListener("click", (e) => {
-        //agarra el LI del boton eleminar
-        const item = e.target.parentElement;
-        ul.removeChild(item);
+        const task = e.target.parentElement;
+        ul.removeChild(task);
+        let tasks = JSON.parse( localStorage.getItem('tasks'))
+        tasks.splice(e, 1);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        console.log(tasks);
 
         const items = document.querySelectorAll("li");
-
-        if (items.length === 0){
+        if (items.length == 0){
             empty.style.display = "block";
         }
     });
     return deleteBtn;
+}
+
+function showTasks(){
+    document.addEventListener('DOMContentLoaded',() => {
+        tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.forEach(task => {
+            const li = document.createElement('li');
+            const p = document.createElement('p');
+            p.innerHTML = `${task.text}`;
+            li.appendChild(p);
+            li.setAttribute("id", `${task.text}`);
+            li.appendChild(DeleteBtn());
+            ul.appendChild(li);
+            empty.style.display = "none";
+        });
+    });
 }
